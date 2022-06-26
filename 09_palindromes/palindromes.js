27ce2308813cf3685b38bfd6d72659b1e3ec45f8
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-06-26 12:51:18 +0800

    solution for 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+  processed = words.toLowerCase().replace(/[^a-z]/g, "");
+  reversed = processed.split("").reverse().join("");
+  if (processed == reversed) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
