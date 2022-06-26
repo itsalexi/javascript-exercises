da0e54ea02ed9baf2c6d9f7e7adc13ce15231b02
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-06-26 12:07:44 +0800

    solution to 06 leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,23 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 == 0) {
+    if (year % 100 == 0) {
+      if (year % 400 == 0) {
+        return true;
+      }
+      return false;
+    }
+    return true;
+  }
+  return false;
 };
+
+// if YEAR is divisible by 4
+//     if divisible by 100
+//         check if its divisible by 400
+//             = LEAP YEAR
+//         else NOT LEAP YEAR
+//     else not LEAP YEAR
+// LEAP YEAR
 
 // Do not edit below this line
 module.exports = leapYears;
