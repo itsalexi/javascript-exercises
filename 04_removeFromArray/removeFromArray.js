ea2a1fb01e4b608e2a4d5fdfe105a6d742f006b3
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-06-26 11:38:00 +0800

    04 remove from arra y solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...target) {
+  console.log(target);
 
+  return array.filter((x) => !target.includes(x));
 };
 
 // Do not edit below this line
