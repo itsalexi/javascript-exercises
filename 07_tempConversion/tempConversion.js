d58c7800b29c0d3de00c23595eedd50c60b12cdb
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-06-26 12:14:56 +0800

    solution to 07 tempconversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function (temp) {
+  result = (temp - 32) * (5 / 9);
+  return +result.toFixed(1);
 };
 
-const ctof = function() {
-
+const ctof = function (temp) {
+  result = temp * (9 / 5) + 32;
+  return +result.toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
