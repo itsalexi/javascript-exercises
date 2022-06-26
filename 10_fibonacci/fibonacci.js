f2cefa1e5c300f0eefa0c2163e5fb4d46636adbc
Alexi <108108603+itsalexi@users.noreply.github.com>
2022-06-26 13:01:27 +0800

    solution to 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function (nth) {
+  num = parseInt(nth);
+  if (num < 0) return "OOPS";
 
+  const goldenRatio = 1.618034;
+  return Math.floor(
+    (goldenRatio ** num - (1 - goldenRatio) ** num) / Math.sqrt(5)
+  );
 };
 
 // Do not edit below this line
